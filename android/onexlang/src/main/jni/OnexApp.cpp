69d7f8b14b81bc0c6af87956a4a7ce73ce114f27
DuncanCragg <duncan@cilux.org>
2020-04-07 21:11:39 +0100

    libs

--- android/onexlang/src/main/jni/OnexApp.cpp
+++ android/onexlang/src/main/jni/OnexApp.cpp
@@ -12,6 +12,7 @@ extern void run_light_tests();
 extern void run_evaluate_object_setter_tests();
 extern void run_evaluate_edit_rule_tests();
 extern void run_device_tests();
+extern void run_clock_tests();
 }
 
 android_app* androidApp;
@@ -82,6 +83,7 @@ public:
     run_evaluate_object_setter_tests();
     run_evaluate_edit_rule_tests();
     run_device_tests();
+    run_clock_tests();
 
     int failures=onex_assert_summary();
 
