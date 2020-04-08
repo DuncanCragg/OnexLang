8772bb4ae267f1c4beaa5891f3cdf3051d5e7d50
DuncanCragg <duncan@cilux.org>
2020-04-08 11:47:24 +0100

    pinetime tests

--- tests/main.c
+++ tests/main.c
@@ -1,8 +1,13 @@
 
 #if defined(NRF5)
 #include <boards.h>
+#if defined(BOARD_PINETIME)
+#include <onex-kernel/gfx.h>
+#endif
 #include <onex-kernel/gpio.h>
+#if defined(HAS_SERIAL)
 #include <onex-kernel/serial.h>
+#endif
 #include <onex-kernel/blenus.h>
 #endif
 #include <onex-kernel/time.h>
@@ -33,9 +38,24 @@ int main(void)
   time_init();
 #if defined(NRF5)
   gpio_init();
+#if defined(HAS_SERIAL)
   serial_init((serial_recv_cb)on_recv,0);
   blenus_init(0);
   time_ticker(serial_loop, 1);
+#else
+  blenus_init((blenus_recv_cb)on_recv);
+#if defined(BOARD_PINETIME)
+  gfx_reset();
+  gfx_init();
+  gfx_screen_colour(0x0);
+  gfx_screen_fill();
+  gfx_pos(10, 10);
+  gfx_text_colour(GFX_BLUE);
+  gfx_text("OnexLang");
+  gpio_mode(LCD_BACKLIGHT_HIGH, OUTPUT);
+  gpio_set(LCD_BACKLIGHT_HIGH, LEDS_ACTIVE_STATE);
+#endif
+#endif
   while(1) run_tests_maybe();
 #else
   on_recv((unsigned char*)"t", 1);
@@ -58,6 +78,15 @@ void run_tests_maybe()
   run_device_tests();
   run_clock_tests();
 
+#if defined(NRF5)
+  int failures=onex_assert_summary();
+#if defined(BOARD_PCA10059)
+#elif defined(BOARD_PINETIME)
+  gfx_pos(10, 10);
+  gfx_text(failures? "FAIL": "SUCCESS");
+#endif
+#else
   onex_assert_summary();
+#endif
 }
 
