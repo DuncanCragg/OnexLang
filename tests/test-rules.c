40206e0d8255ae113e2be25aff0c0ddcb9cb1151
DuncanCragg <duncan@cilux.org>
2023-03-03 15:08:41 +0000

    clear edit after each test

--- tests/test-rules.c
+++ tests/test-rules.c
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <inttypes.h>
 #include <tests.h>
 #include <onex-kernel/log.h>
@@ -37,18 +38,20 @@ void run_evaluate_edit_rule_tests() {
   onex_assert_equal(object_property(target, "fruits:2"), "orange",   "evaluate_edit_rule set fruits to 'mango orange fig'");
   onex_assert_equal(object_property(target, "fruits:3"), "fig",      "evaluate_edit_rule set fruits to 'mango orange fig'");
 
+  object_property_set(edit, "fruits", 0);
+
   // ----- delete from list -------
 
-  object_property_set(edit, "fruits", 0);
   object_property_set(edit, "fruits\\:2", "=>");
   onex_loop();
   onex_assert_equal(object_property(target, "fruits:1"), "mango",    "evaluate_edit_rule deletes 'orange'");
   onex_assert_equal(object_property(target, "fruits:2"), "fig",      "evaluate_edit_rule deletes 'orange'");
+  object_property_set(edit, "fruits\\:2", 0);
 
   object_property_set(edit, "fruits", "=>");
-  object_property_set(edit, "fruits\\:2", 0);
   onex_loop();
   onex_assert(     !object_property(target, "fruits"),               "evaluate_edit_rule deletes all fruit");
+  object_property_set(edit, "fruits", 0);
 
   // ----- set in list -------
 
@@ -58,28 +61,26 @@ void run_evaluate_edit_rule_tests() {
   onex_loop();
   object_property_set(edit, "fruits", "=> fig @. apple");
   onex_loop();
-
   onex_assert_equal(object_property(target, "fruits:1"), "fig",    "evaluate_edit_rule sets fruits:1 to fig");
   onex_assert_equal(object_property(target, "fruits:2"), "orange", "evaluate_edit_rule sets fruits:2 to orange");
   onex_assert_equal(object_property(target, "fruits:3"), "mango",  "evaluate_edit_rule sets fruits:3 to mango");
   onex_assert_equal(object_property(target, "fruits:4"), "banana", "evaluate_edit_rule sets fruits:4 to banana");
   onex_assert_equal(object_property(target, "fruits:5"), "papaya", "evaluate_edit_rule sets fruits:5 to papaya");
   onex_assert_equal(object_property(target, "fruits:6"), "apple",  "evaluate_edit_rule sets fruits:6 to apple");
-
   object_property_set(edit, "fruits", 0);
+
   object_property_set(edit, "fruits\\:1", "=> potato");
   object_property_set(edit, "fruits\\:3", "=>");
   onex_loop();
-
   onex_assert_equal(object_property(target, "fruits:1"), "potato", "evaluate_edit_rule sets fruits:1 to potato");
   onex_assert_equal(object_property(target, "fruits:2"), "orange", "evaluate_edit_rule leaves value");
   onex_assert_equal(object_property(target, "fruits:3"), "banana", "evaluate_edit_rule has deleted mango");
   onex_assert_equal(object_property(target, "fruits:4"), "papaya", "evaluate_edit_rule leaves value");
+  object_property_set(edit, "fruits\\:1", 0);
+  object_property_set(edit, "fruits\\:3", 0);
 
   // ----- prepend in list -------
 
-  object_property_set(edit, "fruits\\:1", 0);
-  object_property_set(edit, "fruits\\:3", 0);
   object_property_set(edit, "fruits", "=> tangerine @.");
   onex_loop();
   onex_assert_equal(object_property(target, "fruits:1"), "tangerine", "evaluate_edit_rule set fruits to 'tangerine potato'");
@@ -90,6 +91,7 @@ void run_evaluate_edit_rule_tests() {
   onex_assert_equal(object_property(target, "fruits:1"), "grapes",    "evaluate_edit_rule set fruits to 'grapes tangerine potato'");
   onex_assert_equal(object_property(target, "fruits:2"), "tangerine", "evaluate_edit_rule set fruits to 'grapes tangerine potato'");
   onex_assert_equal(object_property(target, "fruits:3"), "potato",    "evaluate_edit_rule set fruits to 'grapes tangerine potato'");
+  object_property_set(edit, "fruits", 0);
 
 }
 
